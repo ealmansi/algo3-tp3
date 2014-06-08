edba428031843a7dc6fc781f5a58f33acf065bb3
ealmansi <ealmansi@gmail.com>
2014-06-08 14:34:34 -0300

    cambio cacm::salida; ahora la solucion esta formada por una lista de ejes (con pesos) y no una lista de nodos

--- cpp/src/busq_local.cpp
+++ cpp/src/busq_local.cpp
@@ -10,33 +10,6 @@ using namespace std;
 typedef list<int> li;
 typedef vector<bool> vb;
 
-struct operacion
-{
-  typedef enum {OP_AGREGAR, OP_BORRAR, OP_CAMBIAR, NINGUNA} tipo_op;
-  operacion(tipo_op tipo = NINGUNA) : tipo(tipo) {}
-  tipo_op tipo;
-};
-
-struct op_agregar : operacion
-{
-  op_agregar(leje::iterator& pos, eje u_x, eje x_v)
-    : operacion(OP_AGREGAR), pos(pos), u_x(u_x), x_v(x_v) {}
-  leje::iterator& pos;
-  eje u_x, x_v;
-};
-
-struct op_borrar : operacion
-{
-  op_borrar(int pos) : operacion(OP_BORRAR), pos(pos) {}
-  int pos;
-};
-
-struct op_cambiar : operacion
-{
-  op_cambiar(int pos, int v) : operacion(OP_CAMBIAR), pos(pos), v(v) {}
-  int pos, v;
-};
-
 /*    *    *    *    *    *    *    *    *    *    *    *    */
 
 salida resolver(const entrada& e)
