72d55a4b0eba58cee2ca63f6da7da647b1fc07d9
ealmansi <ealmansi@gmail.com>
2014-06-08 16:58:55 -0300

    busqueda local: implementado recorrer vecinos tipo 1, 2 y 3 (no se bien como testear el codigo)

--- cpp/src/cacm_busq_local_resolver.cpp
+++ cpp/src/cacm_busq_local_resolver.cpp
@@ -74,6 +74,82 @@ void buscar_vecinos_tipo_1(solucion& sol, vecino& mejor_vecino, const entrada& e
   }
 }
 
+// vecinos formados al cambiar (u,v)(v,z) por (u,z)
+void buscar_vecinos_tipo_2(solucion& sol, vecino& mejor_vecino, const entrada& e)
+{
+  // para cada par de ejes (u,v)(v,z) en la solucion
+  leje::iterator it_uv = sol.ejes.begin(), it_vz = sol.ejes.begin(); ++it_vz;
+  for (; it_vz != sol.ejes.end(); ++it_uv, ++it_vz)
+  {
+    eje& uv = *it_uv,
+    vz = *it_vz;
+
+    // busco a z entre los adyacentes de u
+    lady::const_iterator it_ady_u;
+    for (it_ady_u = e.adyacentes[uv.u].begin(); it_ady_u != e.adyacentes[uv.u].end(); ++it_ady_u)
+    {
+      const ady& ady_u = *it_ady_u;
+
+      if (ady_u.v != vz.v) continue;
+
+      // actualizo mejor_vecino si esta solucion vecina es valida y es mejor
+      int W1_prima = sol.W1 - uv.w1 - vz.w1 + ady_u.w1,
+      W2_prima = sol.W2 - uv.w2 - vz.w2 + ady_u.w2;
+      if (W1_prima <= e.K and (mejor_vecino.tipo == vecino::V_INVALIDO or W2_prima < mejor_vecino.W2))
+      {
+        mejor_vecino.tipo = vecino::V_TIPO_2;
+        mejor_vecino.pos = it_uv;
+        mejor_vecino.ejes_nuevos.clear();
+        mejor_vecino.ejes_nuevos.push_back(eje(uv.u, ady_u.v, ady_u.w1, ady_u.w2));
+        mejor_vecino.W1 = W1_prima;
+        mejor_vecino.W2 = W2_prima;
+      }
+
+      break;
+    }
+  }
+}
+
+// vecinos formados al cambiar (u,v)(v,z) por (u,x)(x,z)
+void buscar_vecinos_tipo_3(solucion& sol, vecino& mejor_vecino, const entrada& e)
+{
+  // para cada par de ejes (u,v)(v,z) en la solucion
+  leje::iterator it_uv = sol.ejes.begin(), it_vz = sol.ejes.begin(); ++it_vz;
+  for (; it_vz != sol.ejes.end(); ++it_uv, ++it_vz)
+  {
+    eje& uv = *it_uv,
+    vz = *it_vz;
+
+    // para cada vertice adyacente a `u` y a `z`
+    lady::const_iterator it_ady_u = e.adyacentes[uv.u].begin(), it_ady_z = e.adyacentes[vz.v].begin();
+    while (it_ady_u != e.adyacentes[uv.u].end() and it_ady_z != e.adyacentes[vz.v].end())
+    {
+      const ady& ady_u = *it_ady_u, ady_z = *it_ady_z;
+
+      if (ady_u.v < ady_z.v) ++it_ady_u;
+      else if (ady_z.v < ady_u.v) ++it_ady_z;
+      else
+      {
+        // actualizo mejor_vecino si esta solucion vecina es valida y es mejor
+        int W1_prima = sol.W1 - uv.w1 - vz.w1 + ady_u.w1 + ady_z.w1,
+        W2_prima = sol.W2 - uv.w2 - vz.w2 + ady_u.w2 + ady_z.w2;
+        if (W1_prima <= e.K and (mejor_vecino.tipo == vecino::V_INVALIDO or W2_prima < mejor_vecino.W2))
+        {
+          mejor_vecino.tipo = vecino::V_TIPO_3;
+          mejor_vecino.pos = it_uv;
+          mejor_vecino.ejes_nuevos.clear();
+          mejor_vecino.ejes_nuevos.push_back(eje(uv.u, ady_u.v, ady_u.w1, ady_u.w2));
+          mejor_vecino.ejes_nuevos.push_back(eje(ady_z.v, vz.v, ady_z.w1, ady_z.w2));
+          mejor_vecino.W1 = W1_prima;
+          mejor_vecino.W2 = W2_prima;
+        }
+
+        ++it_ady_u, ++it_ady_z;
+      }
+    }
+  }
+}
+
 salida cacm_busq_local::resolver(const entrada& e)
 {
   salida s_golsoso = cacm_goloso::resolver(e);
@@ -87,6 +163,8 @@ salida cacm_busq_local::resolver(const entrada& e)
   {
     vecino mejor_vecino;
     buscar_vecinos_tipo_1(sol, mejor_vecino, e);
+    buscar_vecinos_tipo_2(sol, mejor_vecino, e);
+    buscar_vecinos_tipo_3(sol, mejor_vecino, e);
 
     if (mejor_vecino.tipo == vecino::V_INVALIDO or not mejor_vecino.W2 < sol.W2)
       seguir_buscando = false;
@@ -97,11 +175,21 @@ salida cacm_busq_local::resolver(const entrada& e)
       case vecino::V_TIPO_1:
         mejor_vecino.pos = sol.ejes.erase(mejor_vecino.pos);
         sol.ejes.insert(mejor_vecino.pos, range(mejor_vecino.ejes_nuevos));
-        sol.W1 = mejor_vecino.W1;
-        sol.W2 = mejor_vecino.W2;
+        break;
+      case vecino::V_TIPO_2:
+        mejor_vecino.pos = sol.ejes.erase(mejor_vecino.pos);
+        mejor_vecino.pos = sol.ejes.erase(mejor_vecino.pos);
+        sol.ejes.insert(mejor_vecino.pos, range(mejor_vecino.ejes_nuevos));
+        break;
+      case vecino::V_TIPO_3:
+        mejor_vecino.pos = sol.ejes.erase(mejor_vecino.pos);
+        mejor_vecino.pos = sol.ejes.erase(mejor_vecino.pos);
+        sol.ejes.insert(mejor_vecino.pos, range(mejor_vecino.ejes_nuevos));
         break;
       }
     }
+    sol.W1 = mejor_vecino.W1;
+    sol.W2 = mejor_vecino.W2;
   }
 
   return s_golsoso;
