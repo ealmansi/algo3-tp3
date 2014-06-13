f6c6f39baab7fa16a913fadde2c0932e5bd2a5fd
fcanay <fcanay@hotmail.com>
2014-06-13 19:09:47 -0300

    grasp funcionando

--- cpp/src/cacm_goloso.h
+++ cpp/src/cacm_goloso.h
@@ -2,12 +2,19 @@
 #define CACM_H_GOLOSO
 
 #include "cacm.h"
+#include <vector>
 #define NULL 0
+
+using namespace std;
+
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<double> vd;
 
 namespace cacm_goloso
 {
   cacm::salida resolver(const cacm::entrada& e, int cant_it, double* p = NULL);
-  void dijkstra(const cacm::entrada& e, vector<bool>& visitado, vector<double>& dist, vector<int>& dist_w1, vector<int>& dist_w2, vector<int>& pred, double alfa);
+  void dijkstra(const cacm::entrada& e, vb& visitado, vd& dist, vi& dist_w1, vi& dist_w2, vi& pred, double alfa);
 };
  
 #endif /* CACM_H_GOLOSO */
