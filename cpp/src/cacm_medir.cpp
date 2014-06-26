ebc0e2827077a6c80dd59fdd3139c1b423bc51a0
fcanay <fcanay@hotmail.com>
2014-06-26 18:02:15 -0300

    princpio graf proporcion golosa

--- cpp/src/cacm_medir.cpp
+++ cpp/src/cacm_medir.cpp
@@ -61,6 +61,72 @@ entrada generar_instancia_aleatoria(int n, int m, int max_w1, int max_w2, int K)
   return e;
 }
 
+entrada grafo_rompe_goloso(int n, int i	) //n mayor que 5
+{
+  entrada e;
+  e.n = n;
+  e.m = n+1;
+  e.u = 1;
+  e.v = n - ((n-2) % 3);
+  int k = ((n-2)/ 3);
+  int proporcion = 2*i-1;
+  e.K = proporcion*(k+1) ;
+  //i = i / k;
+  //if(i == 0){i++;}
+  //if((i % k) != 0){i++;}
+  int q = 3;//k==1 ? ((k+i-1)/ k) +1 : ((k+i-1)/ (k-1)) +1;
+  
+  //cout << " K: " << k << " q: " << q << endl;
+  e.adyacentes.resize(e.n+1);
+  int j = e.u+1;
+  // Primer Camino, mas que la cota
+  
+  e.adyacentes[e.u].push_back(ady(2,proporcion+1,1));
+  e.adyacentes[2].push_back(ady(e.u,proporcion+1,1));
+  
+  for(int p = 0; p < k-1; p++)
+  {
+  	e.adyacentes[j].push_back(ady(j+1,proporcion+1,1));
+  	e.adyacentes[j+1].push_back(ady(j,proporcion+1,1));
+  	j++;
+  }
+  
+  e.adyacentes[j].push_back(ady(e.v,proporcion+1,1));
+  e.adyacentes[e.v].push_back(ady(j,proporcion+1,1));
+  
+  j++;
+  
+  //segundo camino, optimo
+  e.adyacentes[e.u].push_back(ady(j,proporcion,q));
+  e.adyacentes[j].push_back(ady(e.u,proporcion,q));
+  
+  for(int p = 0; p < k-1; p++)
+  {
+  	e.adyacentes[j].push_back(ady(j+1,proporcion,q));
+  	e.adyacentes[j+1].push_back(ady(j,proporcion,q));
+  	j++;
+  }
+  
+  e.adyacentes[j].push_back(ady(e.v,proporcion,q));
+  e.adyacentes[e.v].push_back(ady(j,proporcion,q));
+  
+  j++;
+  //tercer camino, sub-optimo valido
+  e.adyacentes[e.u].push_back(ady(j,1,q*i));
+  e.adyacentes[j].push_back(ady(e.u,1,q*i));
+  for(int p = 0; p < k-1 ; p++)
+  {
+  	e.adyacentes[j].push_back(ady(j+1,1,q*i));
+  	e.adyacentes[j+1].push_back(ady(j,1,q*i));
+  	j++;
+  }
+  
+  e.adyacentes[j].push_back(ady(e.v,1,q*i));
+  e.adyacentes[e.v].push_back(ady(j,1,q*i));
+  
+  return e;
+}
+
 // typedef long double ld;
 // typedef vector<ld> vld;
 // #define range(c) (c).begin(), (c).end()
@@ -117,6 +183,7 @@ typedef vector<vlli> vvlli;
 // gráfico tiempo vs n (para distintos m si queda bien)
 void medir_exacto(int n_min, int n_max)
 {
+	cout << "Medicion exacto" << endl;
   timespec inicio, fin;
   vvlli mediciones_por_n(n_max - n_min + 1, vlli(CANT_MEDICIONES_POR_N));
 
@@ -147,8 +214,38 @@ void medir_exacto(int n_min, int n_max)
   }
 }
 
+void medir_goloso_proporcion(int n_min, int n_max)
+{
+	cout << "Medicion goloso proporcion" << endl;
+	salida s;
+	entrada e;
+	double proporcion;
+	for(int i = n_min; i < n_max; i += 3 ){
+		//cout << "n: " << i << endl;
+		//cout << "i: " << (i-2)/3+1 << endl;
+		entrada e = grafo_rompe_goloso(i,i*i);
+
+    
+    //cout << "goloso" << endl;
+    s = cacm_goloso::resolver(e);
+    //escribir_salida(s);
+    //cout << endl;	
+		proporcion = s.W2;
+		
+		//cout << "exacto" << endl;
+    s = cacm_exacto::resolver(e);
+    //escribir_salida(s);
+    //cout << endl;
+		proporcion /= s.W2;
+		
+		cout << i << " " << proporcion << endl;
+		
+	}}
+
 int main(int argc, char const *argv[])
 {
-  medir_exacto(3, 13);
+  
+  //medir_exacto(3, 13);
+  medir_goloso_proporcion(8, 100);
   return 0;
 }
