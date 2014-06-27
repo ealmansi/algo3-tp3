548e532970004c5f825d38dd19fd2fc117cc671f
fcanay <fcanay@hotmail.com>
2014-06-27 02:09:05 -0300

    familia que rompe goloso pero no busqueda local

--- cpp/src/cacm_tester.cpp
+++ cpp/src/cacm_tester.cpp
@@ -77,6 +77,102 @@ entrada grafo_rompe_goloso(int n, int i	) //n mayor que 5
   return e;
 }
 
+entrada grafo_rompe_goloso2(int n, int i ) //n mayor que 11 y cada 6
+{
+  entrada e;
+  e.n = n;
+  e.m = n+1;
+  e.u = 1;
+  e.v = n - ((n-2) % 3);
+  int k = ((n-2)/ 3);
+  int proporcion = 2*i-1;
+  e.K = proporcion*(k+1) ;
+  //i = i / k;
+  //if(i == 0){i++;}
+  //if((i % k) != 0){i++;}
+  int q = 3;//k==1 ? ((k+i-1)/ k) +1 : ((k+i-1)/ (k-1)) +1;
+  
+  //cout << " K: " << k << " q: " << q << endl;
+  e.adyacentes.resize(e.n+1);
+  int j = e.u+1;
+  // Primer Camino, mas que la cota
+  
+  e.adyacentes[e.u].push_back(ady(2,proporcion+1,1));
+  e.adyacentes[2].push_back(ady(e.u,proporcion+1,1));
+  
+  for(int p = 0; p < k-1; p++)
+  {
+    e.adyacentes[j].push_back(ady(j+1,proporcion+1,1));
+    e.adyacentes[j+1].push_back(ady(j,proporcion+1,1));
+    j++;
+  }
+  
+  e.adyacentes[j].push_back(ady(e.v,proporcion+1,1));
+  e.adyacentes[e.v].push_back(ady(j,proporcion+1,1));
+  
+  j++;
+  
+  //segundo camino, optimo
+  e.adyacentes[e.u].push_back(ady(j,proporcion,q));
+  e.adyacentes[j].push_back(ady(e.u,proporcion,q));
+  
+  for(int p = 0; p < k-1; p++)
+  {
+    e.adyacentes[j].push_back(ady(j+1,proporcion,q));
+    e.adyacentes[j+1].push_back(ady(j,proporcion,q));
+    j++;
+  }
+  
+  e.adyacentes[j].push_back(ady(e.v,proporcion,q));
+  e.adyacentes[e.v].push_back(ady(j,proporcion,q));
+  
+  j++;
+  //tercer camino, sub-optimo valido
+  int g = j;
+  e.adyacentes[e.u].push_back(ady(j,1,q*i));
+  e.adyacentes[j].push_back(ady(e.u,1,q*i));
+  for(int p = 0; p < k-1 ; p++)
+  {
+    e.adyacentes[j].push_back(ady(j+1,1,q*i));
+    e.adyacentes[j+1].push_back(ady(j,1,q*i));
+    j++;
+  }
+  
+  e.adyacentes[j].push_back(ady(e.v,1,q*i));
+  e.adyacentes[e.v].push_back(ady(j,1,q*i));
+  
+  //cout << g << " adyacente con: " << g-k+1 << endl;
+  e.adyacentes[g].push_back(ady(g-k+1,proporcion,q*i-1));
+  e.adyacentes[g-k+1].push_back(ady(g,proporcion,q*i-1));
+
+  g+=2;
+  for(int p = 0; p < k-4 ; p+=2)
+  {
+    //cout << g << " adyacente con: " << g-k+1 << " y " << g-k-1 << endl;
+    e.adyacentes[g].push_back(ady(g-k+1,proporcion,q*i-1));
+    e.adyacentes[g-k+1].push_back(ady(g,proporcion,q*i-1));
+
+    e.adyacentes[g].push_back(ady(g-k-1,proporcion,q*i-1));
+    e.adyacentes[g-k-1].push_back(ady(g,proporcion,q*i-1));
+
+    g+=2;
+  }
+  //cout << g << " adyacente con: " << g-k-1 ;
+  e.adyacentes[g].push_back(ady(g-k-1,proporcion,q*i-1));
+  e.adyacentes[g-k-1].push_back(ady(g,proporcion,q*i-1));
+  if(k % 2 == 0)
+  {
+    //cout << " y " << g-k+1 ;
+    e.adyacentes[g].push_back(ady(g-k+1,proporcion,q*i-1));
+    e.adyacentes[g-k+1].push_back(ady(g,proporcion,q*i-1));
+  }
+  //cout << endl;
+  for (int i = 1; i < e.n + 1; ++i)
+    e.adyacentes[i].sort(comparar_ady_por_v);
+
+  return e;
+}
+
 void Test_rompe_goloso(int n, int intervalo){
 	salida s;
 	entrada e;
@@ -105,6 +201,43 @@ void Test_rompe_goloso(int n, int intervalo){
 
 
 }
+
+void Test_rompe_goloso2(int n, int intervalo){
+  salida s;
+  entrada e;
+  double proporcion,proporcion2;
+  for(int i = 8; i < n; i += intervalo ){
+    cout << "n: " << i << endl;
+    //cout << "i: " << (i-2)/3+1 << endl;
+    entrada e = grafo_rompe_goloso2(i,i*i);
+
+    
+    s = cacm_goloso::resolver(e);
+    //cout << "goloso" << endl;
+    //escribir_salida(s);
+    //cout << endl; 
+    proporcion = s.W2;
+
+    s = cacm_busq_local::resolver(e);
+    // cout << "busqueda local" << endl;
+    // escribir_salida(s);
+    // cout << endl; 
+    proporcion2 = s.W2;
+    
+    s = cacm_exacto::resolver(e);
+    //cout << "exacto" << endl;
+    //escribir_salida(s);
+    //cout << endl;
+    proporcion /= s.W2;
+    proporcion2 /= s.W2;
+
+    cout << "Proporcion 1: " << proporcion  << endl;
+    cout << "Proporcion 1: " << proporcion2  << endl << endl;
+  }
+
+
+}
+
 
 void Test_grasp(int n, int intervalo){
 	salida s;
@@ -138,7 +271,7 @@ void Test_grasp(int n, int intervalo){
 int main(int argc, char const *argv[])
 {
   
-  Test_rompe_goloso(6,3);
+  Test_rompe_goloso2(40,3);
   //Test_grasp(1000,3);
   /*entrada e = grafo_rompe_goloso(9,100);
   salida s;
